1e64d9a9004ac196e34a8573a7401e9b74ae1eaf
matt_p <m.peru@directio.it>
2023-03-20 08:29:18 +0100

    0.0.2

--- src/hooks/useVideoPlayer.tsx
+++ src/hooks/useVideoPlayer.tsx
@@ -1,96 +1,111 @@
-import React, { useState, useRef, useEffect, ChangeEvent } from 'react';
+import React, { useState, useRef, useEffect, ChangeEvent, MouseEventHandler } from 'react';
 
 const useVideoPlayer = () => {
-    const [playerState, setPlayerState] = useState({
-      isPlaying: false,
-      progress: 0,
-      speed: 1,
-      isMuted: false,
-    });
-  
-    const videoElement = useRef<HTMLVideoElement | null>(null);
-  
-    const videoCallbackRef: React.RefCallback<HTMLVideoElement> = (element: HTMLVideoElement | null) => {
-      if (element) {
-        console.log('executed because the HTML video element was set.');
-        /* do stuff that requires the element. */
-        videoElement.current = element;
-      }
+  const [playerState, setPlayerState] = useState({
+    isPlaying: false,
+    progress: 0,
+    speed: 1,
+    isMuted: false,
+  });
+
+  const videoElement = useRef<HTMLVideoElement | null>(null);
+
+  const videoCallbackRef: React.RefCallback<HTMLVideoElement> = (element: HTMLVideoElement | null) => {
+    if (element) {
+      console.log('executed because the HTML video element was set.');
+      /* do stuff that requires the element. */
+      videoElement.current = element;
     }
-  
-    useEffect(() => {
-      if (videoElement.current) {
-        console.log('executed because playerState.isPlaying changed.');
-        playerState.isPlaying
-          ? videoElement.current?.play()
-          : videoElement.current?.pause();
-      }
-      
-    }, [playerState.isPlaying]);
-  
-    const togglePlay = () => {
-      setPlayerState({
-        ...playerState,
-        isPlaying: !playerState.isPlaying,
-      });
-    };
-  
-    const handleOnTimeUpdate = () => {
-      const progress = ((videoElement.current?.currentTime ?? 1) / (videoElement.current?.duration ?? 1)) * 100;
+  }
+
+  useEffect(() => {
+    if (videoElement.current) {
+      console.log('executed because playerState.isPlaying changed.');
+      playerState.isPlaying
+        ? videoElement.current?.play()
+        : videoElement.current?.pause();
+    }
+
+  }, [playerState.isPlaying]);
+
+  const togglePlay = () => {
+    setPlayerState({
+      ...playerState,
+      isPlaying: !playerState.isPlaying,
+    });
+  };
+
+  const handleOnTimeUpdate = () => {
+    if (videoElement.current) {
+      const progress = (videoElement.current.currentTime / videoElement.current.duration) * 100;
       setPlayerState({
         ...playerState,
         progress,
       });
-    };
-  
-    const handleVideoProgress = (event: ChangeEvent<HTMLInputElement>) => {
-      const manualChange = Number(event.target.value);
-      if (videoElement.current) {
-        videoElement.current.currentTime = ((videoElement.current?.duration ?? 0) / 100) * manualChange;
-        setPlayerState({
-          ...playerState,
-          progress: manualChange,
-        });
-      }
-    };
-  
-    const handleVideoSpeed = (event: ChangeEvent<HTMLSelectElement>) => {
-      const speed = Number(event.target.value);
-      if (videoElement.current) {
-        videoElement.current.playbackRate = speed;
-  
-        setPlayerState({
-          ...playerState,
-          speed,
-        });
-      }
-    };
-  
-    const toggleMute = () => {
+    }
+  };
+
+  const handleVideoProgressClick = (event: MouseEventHandler<HTMLInputElement>) => {
+    const d = event.apply;
+    const manualChange = Number();
+    if (videoElement.current) {
+      videoElement.current.currentTime = (videoElement.current.duration / 100) * manualChange;
       setPlayerState({
         ...playerState,
-        isMuted: !playerState.isMuted,
+        progress: manualChange,
       });
-    };
-  
-    useEffect(() => {
-      console.log('executed because playerState.isMuted changed.');
-      if (videoElement.current) {
-        playerState.isMuted
-          ? (videoElement.current.muted = true)
-          : (videoElement.current.muted = false);
-      }
-    }, [playerState.isMuted]);
-  
-    return {
-      playerState,
-      togglePlay,
-      handleOnTimeUpdate,
-      handleVideoProgress,
-      handleVideoSpeed,
-      toggleMute,
-      ref: videoCallbackRef
-    };
+    }
+  };
+
+  const handleVideoProgress = (event: ChangeEvent<HTMLInputElement>) => {
+    const manualChange = Number(event.target.value);
+    if (videoElement.current) {
+      videoElement.current.currentTime = (videoElement.current.duration / 100) * manualChange;
+      setPlayerState({
+        ...playerState,
+        progress: manualChange,
+      });
+    }
+  };
+
+  const handleVideoSpeed = (event: ChangeEvent<HTMLSelectElement>) => {
+    const speed = Number(event.target.value);
+    if (videoElement.current) {
+      videoElement.current.playbackRate = speed;
+
+      setPlayerState({
+        ...playerState,
+        speed,
+      });
+    }
+  };
+
+  const toggleMute = () => {
+    setPlayerState({
+      ...playerState,
+      isMuted: !playerState.isMuted,
+    });
+  };
+
+  useEffect(() => {
+    console.log('executed because playerState.isMuted changed.');
+    if (videoElement.current) {
+      playerState.isMuted
+        ? (videoElement.current.muted = true)
+        : (videoElement.current.muted = false);
+    }
+  }, [playerState.isMuted]);
+
+  return {
+    playerState,
+    togglePlay,
+    handleOnTimeUpdate,
+    handleVideoProgress,
+    handleVideoSpeed,
+    toggleMute,
+    handleVideoProgressClick,
+    ref: videoCallbackRef
+  };
 };
 
 export default useVideoPlayer;
